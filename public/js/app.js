93848e7f8c0a8d1e6d62d8a8c9973133024ed974
Alex Maday <alex.maday@gmail.com>
2018-05-21 20:12:08 -0500

    convert keyup listener on #new-todo from jq->pojs

--- public/js/app.js
+++ public/js/app.js
@@ -56,8 +56,8 @@ jQuery(function ($) {
 			}).init('/all');
 		},
 		bindEvents: function () {
-			$('#new-todo').on('keyup', this.create.bind(this));
-			$('#toggle-all').on('change', this.toggleAll.bind(this));		// checkbox
+			document.getElementById('new-todo').addEventListener('keyup', this.create.bind(this));
+			$('#toggle-all').on('change', this.toggleAll.bind(this));
 			$('#footer').on('click', '#clear-completed', this.destroyCompleted.bind(this));
 			$('#todo-list')
 				.on('change', '.toggle', this.toggle.bind(this))
